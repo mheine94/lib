2b5f234167e5b9d8f210ac193dd0da383e4c250a
Max Fechner <max.fechner@gmail.com>
2020-10-15 13:31:34 +0200

    wiki.js

---
+++ wiki.js
@@ -0,0 +1,12 @@
+export class wiki {
+	static async search(language, query) {
+		console.log('search', language, query)
+		let result = await fetch(`https://${language}.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&format=json&origin=*`).then(x => x.json());
+		console.log('search result', result)
+		return result.query.search;
+	}
+	static async page(language, title) {
+		let result = await fetch(`https://${language}.wikipedia.org/w/api.php?action=parse&prop=wikitext&page=${title}&format=json&origin=*`).then(x => x.json())
+		return result.parse.wikitext['*'];
+	}
+}
