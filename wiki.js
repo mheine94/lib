1524b37f2922278aeb978f97285f30c7f23041df
Max Fechner <max.fechner@gmail.com>
2020-11-02 15:06:59 +0100

    category

--- wiki.js
+++ wiki.js
@@ -5,8 +5,26 @@ export default class wiki {
 		// console.log('search result', result)
 		return result?.query?.search;
 	}
+
 	static async page(language, title) {
 		let result = await fetch(`https://${language}.wikipedia.org/w/api.php?action=parse&prop=wikitext&page=${title}&format=json&origin=*`).then(x => x.json())
 		return result?.parse?.wikitext?.['*'];
 	}
+
+	static async category(language, categoryName) {
+		let prefix = {
+			de: 'Kategorie',
+			en: 'Category',
+		}
+		let titles = []
+		let gcmcontinue;
+		do {
+			let result = await fetch(`https://${language}.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=info&generator=categorymembers&gcmtitle=${prefix[language]}:${categoryName}&gcmlimit=max${gcmcontinue ? `&gcmcontinue=${gcmcontinue}` : ""}`).then(x => x.json());
+			titles = [...titles, ...Object.values(result.query.pages).map(x => x.title)]
+			console.log(`loaded ${titles.length} results`)
+			gcmcontinue = result?.continue?.gcmcontinue;
+		} while (gcmcontinue)
+		return titles;
+	}
+
 }
