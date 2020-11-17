fe2c29d2d98ddbdad59a10cfb4629444b83bd1f1
Max Fechner <max.fechner@gmail.com>
2020-11-17 11:11:50 +0100

    - string.js remove prototype assignments

--- string.js
+++ string.js
@@ -1,12 +1,12 @@
-String.prototype.toTitleCase = function () {
-	return this.split(' ')
-		.map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
-		.join(' ')
-}
+// String.prototype.toTitleCase = function () {
+// 	return this.split(' ')
+// 		.map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+// 		.join(' ')
+// }
 
-String.prototype.groups = function (regex) {
-	return Array.from(this.matchAll(regex)).map(i => i.slice(1)).flat();
-}
+// String.prototype.groups = function (regex) {
+// 	return Array.from(this.matchAll(regex)).map(i => i.slice(1)).flat();
+// }
 
 
 
